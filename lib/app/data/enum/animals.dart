7192eef3846440a82d5d82cb58c94ce63c867e29
eric <walkcloud@outlook.com>
2023-08-08 16:15:37 -0700

    opt dir

--- lib/app/data/enum/animals.dart
+++
@@ -1,1 +0,0 @@
-enum Animals { Todos, Gatos, Cachorros }
--- lib/app/modules/learn/binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:lime_english/app/modules/learn/controller.dart';
-
-class LearnBinding implements Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<LearnController>(() => LearnController());
-  }
-}
