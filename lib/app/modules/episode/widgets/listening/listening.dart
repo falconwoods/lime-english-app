8f9056ec23181568383c69221102bdec6c9d0004
eric <walkcloud@outlook.com>
2023-09-03 01:01:45 -0700

    opt

--- lib/app/modules/episode/widgets/listening/listening.dart
+++ lib/app/modules/episode/widgets/listening/listening.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:lime_english/app/modules/episode/widgets/listening_text/listening_text_controller.dart';
 import 'package:lime_english/app/widgets/app_video_player/app_video_player.dart';
 import 'package:lime_english/app/modules/episode/widgets/listening/listening_arg.dart';
 import 'package:lime_english/app/modules/episode/widgets/listening/listening_controller.dart';
@@ -10,8 +9,7 @@ import 'package:video_player/video_player.dart';
 
 class Listening extends GetView<ListeningController> {
   final ListeningArg arg;
-  Listening(this.arg, {Key? key}) : super(key: key);
-  var cnt = 500.0.obs;
+  const Listening(this.arg, {Key? key}) : super(key: key);
 
   void onVideoUpdate(Duration position) {
     var se = controller.primarySubtitle.getSubtitle(position);
